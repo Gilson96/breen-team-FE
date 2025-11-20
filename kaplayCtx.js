5b5c8a76a5cbb9c7631e1671fea33e2a5119b89a
Sathoryn <sathcode@gmail.com>
2025-11-20 16:31:31 +0000

    kaplay start

---
+++ kaplayCtx.js
@@ -0,0 +1,16 @@
+import kaplay from 'kaplay';
+
+export default function initKaplay() {
+  return kaplay({
+    width: 1282,
+    height: 720,
+    letterbox: true,
+    global: false,
+    debug: true, // TODO: put back to false in prod
+    debugKey: 'f1',
+    canvas: document.getElementById('game'),
+    pixelDensity: devicePixelRatio,
+    crisp: true,
+    maxFPS: 60,
+  });
+}
