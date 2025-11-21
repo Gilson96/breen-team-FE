d62a1668b321bc308200b8c528e7f10440d317dd
Dan Moffat <danmoffat@gmail.com>
2025-11-21 14:48:34 +0000

    Add styling changes and CSS reset

--- src/components/ScoreList/ScoreList.tsx
+++ src/components/ScoreList/ScoreList.tsx
@@ -1,6 +1,19 @@
 import ScoreRow from '../ScoreRow/ScoreRow';
 import './ScoreList.css';
 
+const mockScores = [
+  { score: 50, username: 'BHS' },
+  { score: 72, username: 'KLM' },
+  { score: 18, username: 'QRT' },
+  { score: 91, username: 'ZXP' },
+  { score: 44, username: 'MJN' },
+  { score: 63, username: 'TUV' },
+  { score: 27, username: 'WQH' },
+  { score: 85, username: 'RDK' },
+  { score: 39, username: 'FLS' },
+  { score: 58, username: 'CPV' }
+];
+
 const ScoreList = () => {
   return (
     <ul className='scoreList'>
