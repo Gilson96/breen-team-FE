ca093a63352e700fc65d00704a75961eb90ac0e4
Gilson96 <grafael99@gmail.com>
2025-11-21 10:31:48 +0000

    Implement scoreRow component

--- src/components/ScoreList/ScoreList.tsx
+++ src/components/ScoreList/ScoreList.tsx
@@ -1,7 +1,12 @@
+import ScoreRow from '../ScoreRow/ScoreRow';
 import './ScoreList.css';
 
 const ScoreList = () => {
-  return <ul className='scoreList'>ScoreList</ul>;
+  return (
+    <ul className='scoreList'>
+      <ScoreRow />
+    </ul>
+  );
 };
 
 export default ScoreList;
