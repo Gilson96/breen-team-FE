3bb515e6452d7588e284f360e7f0b4afb32fe881
Dan Moffat <danmoffat@gmail.com>
2025-12-01 10:31:34 +0000

    Add getScoresById API route

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,9 +9,9 @@ const HomePage = () => {
       <div className='homePage__title'>
         <Orc />
         <h1>
-          {' '}
-          Burnout <br />
-          <span>Breakers</span>
+          Burnout
+          <br />
+          <span>Break</span>
         </h1>
       </div>
       <HomeButton to='/games' animate>
