1e288885e3e8576362c17e69d795423158c0fe7e
Dan Moffat <danmoffat@gmail.com>
2025-12-01 13:57:13 +0000

    Add account page

---
+++ src/pages/AccountPage.tsx
@@ -0,0 +1,14 @@
+import Heading from '../components/Heading/Heading';
+import Nav from '../components/Nav/Nav';
+import './AccountPage.css';
+
+const AccountPage = () => {
+  return (
+    <main className='account'>
+      <Heading>Your Account</Heading>
+      <Nav />
+    </main>
+  );
+};
+
+export default AccountPage;
