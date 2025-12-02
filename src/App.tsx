a7b8f56446a729aecacf4d2c1cc1f0b7741079f1
Dan Moffat <danmoffat@gmail.com>
2025-12-02 09:35:16 +0000

    Reinstate 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import HomePage from './pages/HomePage';
 import LeaderboardPage from './pages/LeaderboardPage';
 import GamesPage from './pages/GamesPage';
 import AccountPage from './pages/AccountPage';
-// import NotFoundPage from './pages/NotFoundPage';
+import NotFoundPage from './pages/NotFoundPage';
 import Debugger from './components/Debugger/Debugger';
 import Runner from './components/Runner/Runner';
 import MemoryHeap from './components/MemoryGame/MemoryHeap';
@@ -18,7 +18,7 @@ const App = () => {
         <Route path='/leaderboards/:scoreId?' element={<LeaderboardPage />} />
         <Route path='/games' element={<GamesPage />} />
         <Route path='/account/:userId' element={<AccountPage />} />
-        {/* <Route path='*' element={<NotFoundPage />} /> */}
+        <Route path='*' element={<NotFoundPage />} />
       </Route>
       <Route path='/debugger' element={<Debugger />} />
       <Route path='/orcoftherings' element={<Runner />} />
